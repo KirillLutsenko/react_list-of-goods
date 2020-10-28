0118d3c92353a1dc53ab7edb13c3b0ec6a034ca6
KirillLutsenko <mnrovski68@gmail.com>
2020-10-28 20:28:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.e00176eb659ed05819e75991fb721ac2.js"
 );
 
 self.addEventListener('message', (event) => {
